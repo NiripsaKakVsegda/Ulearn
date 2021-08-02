08154091449c0ec4c394a3d650d5630fb2bdbd96
vorkulsky <vorkulsky@skbkontur.ru>
2021-08-02 17:28:00 +0500

    Visits unique index

--- src/Database/Models/Visit.cs
+++ src/Database/Models/Visit.cs
@@ -13,20 +13,20 @@ namespace Database.Models
 
 		[StringLength(64)]
 		[Required]
-		[Index("IDX_Visits_ByCourseSlideAndUser", 3)]
+		[Index("IDX_Visits_ByCourseSlideAndUser", 3, IsUnique = true)]
 		[Index("IDX_Visits_ByCourseAndUser", 2)]
 		public string UserId { get; set; }
 
 		[Required]
 		[StringLength(100)]
-		[Index("IDX_Visits_ByCourseSlideAndUser", 1)]
+		[Index("IDX_Visits_ByCourseSlideAndUser", 1, IsUnique = true)]
 		[Index("IDX_Visits_ByCourseSlideAndTime", 1)]
 		[Index("IDX_Visits_ByCourseAndUser", 1)]
 		public string CourseId { get; set; }
 
 		[Required]
+		[Index("IDX_Visits_ByCourseSlideAndUser", 2, IsUnique = true)]
 		[Index("IDX_Visits_ByCourseSlideAndTime", 2)]
-		[Index("IDX_Visits_ByCourseSlideAndUser", 2)]
 		public Guid SlideId { get; set; }
 
 		///<summary>Первый заход на слайд</summary>
