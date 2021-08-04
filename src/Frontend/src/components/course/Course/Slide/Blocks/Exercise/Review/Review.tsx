d259cf446c7c9fd00bfbd9538d51afb978a46080
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-08-04 15:03:21 +0500

    Added length count in review comment textarea, added transparent color to send button

--- src/Frontend/src/components/course/Course/Slide/Blocks/Exercise/Review/Review.tsx
+++ src/Frontend/src/components/course/Course/Slide/Blocks/Exercise/Review/Review.tsx
@@ -558,6 +558,9 @@ class Review extends React.Component<ReviewProps, ReviewState> {
 						data-tid={ id.toString() }
 						onFocus={ this.selectComment }
 						value={ commentReply }
+						lengthCounter={ reviewPolicyChecker.maxReviewLength }
+						error={ commentReply.length > reviewPolicyChecker.maxReviewLength }
+						showLengthCounter={ commentReply.length > reviewPolicyChecker.maxReviewLength }
 					/>
 				</div>
 				<button
