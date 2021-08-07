83bc5f40400d7a428af179e45be57424172f0615
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-08-07 23:56:00 +0500

    Implemented review queue controller

---
+++ src/Web.Api/Controllers/Review/ReviewQueue.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Database;
+using Database.Models;
+using Database.Repos;
+using Database.Repos.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Ulearn.Common.Api.Models.Responses;
+using Ulearn.Core.Courses.Manager;
+using Ulearn.Web.Api.Models.Responses.Review;
+using Ulearn.Web.Api.Utils;
+using Vostok.Logging.Abstractions;
+
+namespace Ulearn.Web.Api.Controllers.Review
+{
+	[Authorize(Policy = "Instructors")]
+	[Route("review-queue")]
+	public class ReviewQueueController : BaseController
+	{
+		private readonly ISlideCheckingsRepo slideCheckingsRepo;
+		private readonly ControllerUtils controllerUtils;
+
+		public ReviewQueueController(
+			ICourseStorage courseStorage,
+			UlearnDb db,
+			IUsersRepo usersRepo,
+			ControllerUtils controllerUtils,
+			ISlideCheckingsRepo slideCheckingsRepo)
+			: base(courseStorage, db, usersRepo)
+		{
+			this.slideCheckingsRepo = slideCheckingsRepo;
+			this.controllerUtils = controllerUtils;
+		}
+
+		public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			var courseId = (string)context.ActionArguments["courseId"];
+
+			if (!courseStorage.HasCourse(courseId))
+			{
+				context.HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+				context.Result = new JsonResult(new ErrorResponse($"Course {courseId} not found"));
+				return;
+			}
+
+			await base.OnActionExecutionAsync(context, next);
+		}
+
+
+		[HttpGet("{courseId}")]
+		public async Task<ReviewQueueResponse> GetReviewQueueInfo(
+			[FromRoute] string courseId,
+			[FromQuery] List<string> groupsIds = null,
+			[FromQuery] bool done = false,
+			[FromQuery] string userId = "",
+			[FromQuery] Guid? slideId = null
+		)
+		{
+			courseStorage.HasCourse(courseId);
+			const int maxShownQueueSize = 500;
+
+			var filterOptions = await GetManualCheckingFilterOptionsByGroup(courseId, groupsIds);
+
+			if (!string.IsNullOrEmpty(userId))
+				filterOptions.UserIds = new List<string> { userId };
+			if (slideId.HasValue)
+				filterOptions.SlidesIds = new List<Guid> { slideId.Value };
+
+			filterOptions.Count = maxShownQueueSize + 1;
+			filterOptions.OnlyChecked = done;
+
+			var checkings = await GetMergedCheckingQueue(filterOptions);
+
+			return ReviewQueueResponse.Build(checkings);
+		}
+
+		[HttpPost("{courseId}/{submissionId}")]
+		public async Task<ActionResult> LockSubmission([FromRoute] string courseId, [FromRoute] int submissionId)
+		{
+			var checking = await slideCheckingsRepo.FindManualCheckingById<ManualExerciseChecking>(submissionId);
+			if (checking == null)
+				return NotFound(new ErrorResponse($"Submission {submissionId} not found"));
+
+			await slideCheckingsRepo.LockManualChecking(checking, UserId);
+			return Ok($"Submission {submissionId} locked by you for 30 minutes");
+		}
+
+		private async Task<ManualCheckingQueueFilterOptions> GetManualCheckingFilterOptionsByGroup(string courseId, List<string> groupsIds)
+		{
+			return await controllerUtils
+				.GetFilterOptionsByGroup<ManualCheckingQueueFilterOptions>(UserId, courseId, groupsIds);
+		}
+
+		private async Task<List<AbstractManualSlideChecking>> GetMergedCheckingQueue(ManualCheckingQueueFilterOptions filterOptions)
+		{
+			var result = (await slideCheckingsRepo
+					.GetManualCheckingQueue<ManualExerciseChecking>(filterOptions))
+				.Cast<AbstractManualSlideChecking>()
+				.ToList();
+
+			result.AddRange(await slideCheckingsRepo.GetManualCheckingQueue<ManualQuizChecking>(filterOptions));
+
+			result = result
+				.OrderByDescending(c => c.Timestamp)
+				.ToList();
+			if (filterOptions.Count > 0)
+				result = result
+					.Take(filterOptions.Count)
+					.ToList();
+
+			return result;
+		}
+	}
+}
---
+++ src/Web.Api/Models/Responses/Review/ReviewQueueResponse.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Database.Models;
+using Ulearn.Common.Api.Models.Responses;
+
+namespace Ulearn.Web.Api.Models.Responses.Review
+{
+	[DataContract]
+	public class ReviewQueueResponse : SuccessResponse
+	{
+		[DataMember]
+		public List<ReviewQueueItem> Checkings { get; set; }
+
+		public static ReviewQueueResponse Build(IEnumerable<AbstractManualSlideChecking> checkings)
+		{
+			return new ReviewQueueResponse
+			{
+				Checkings = checkings
+					.Select(c => new ReviewQueueItem
+					{
+						SubmissionId = c.Id, 
+						UserId = c.UserId,
+						IsLocked = c.IsLocked,
+					})
+					.ToList(),
+			};
+		}
+	}
+
+	[DataContract]
+	public class ReviewQueueItem
+	{
+		[DataMember]
+		public int SubmissionId { get; set; }
+		
+		[DataMember]
+		public string UserId { get; set; }
+		
+		[DataMember]
+		public bool IsLocked { get; set; }
+	}
+}
