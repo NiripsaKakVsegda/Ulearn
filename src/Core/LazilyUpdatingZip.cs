c1244748a3d4a3a5bd31c118edf78456db318021
vorkulsky <vorkulsky@skbkontur.ru>
2021-08-25 12:42:05 +0500

    LazilyUpdatingZipTests

--- src/Core/LazilyUpdatingZip.cs
+++ src/Core/LazilyUpdatingZip.cs
@@ -92,19 +92,4 @@ namespace Ulearn.Core
 				yield return f;
 		}
 	}
-
-	[TestFixture]
-	public class LazilyUpdatingZip_should
-	{
-		[Test]
-		public void TestGetDirectoriesList()
-		{
-			const string fileName = "directory-1/directory-2/subdirectory/file.txt";
-			var directories = LazilyUpdatingZip.GetDirectoriesList(fileName).ToList();
-			CollectionAssert.Contains(directories, "directory-1");
-			CollectionAssert.Contains(directories, "directory-2");
-			CollectionAssert.Contains(directories, "subdirectory");
-			CollectionAssert.DoesNotContain(directories, "file.txt");
-		}
-	}
 }
