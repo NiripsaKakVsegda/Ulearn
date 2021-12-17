c34b56edc5b8bb63cdb11a058ba654b4220b9c7e
Rozentor <36932390+Rozentor@users.noreply.github.com>
2021-12-17 13:01:11 +0500

    ULEARN-990 added logs on parse error

--- src/RunCheckerJob/ResultParser.cs
+++ src/RunCheckerJob/ResultParser.cs
@@ -17,13 +17,14 @@ namespace RunCheckerJob
 			{
 				var objectStartCommaIndex = stdout.IndexOf('{');
 				var objectEndCommaIndex = stdout.LastIndexOf('}');
-				
+
 				RunningResults result;
 
 				if (objectStartCommaIndex != 0 || objectEndCommaIndex != stdout.Length - 1)
 				{
 					var objectToParse = stdout.Substring(objectStartCommaIndex, objectEndCommaIndex - objectStartCommaIndex + 1);
 					result = JsonConvert.DeserializeObject<RunningResults>(objectToParse);
+					result.Logs = new[] { "При парсинге результата были замечены сторонние символы", "Полный вывод:", stdout, };
 					log.Warn("При парсинге результата были замечены сторонние символы");
 				}
 				else result = JsonConvert.DeserializeObject<RunningResults>(stdout);
@@ -37,7 +38,7 @@ namespace RunCheckerJob
 				log.Warn("Не удалось распарсить результат");
 				return interpretNonJsonOutputAs switch
 				{
-					InterpretNonJsonOutputType.CompilationError => new RunningResults(Verdict.CompilationError) { CompilationOutput = string.Join("\n", new[] { stdout, stderr }.Where(s => !string.IsNullOrWhiteSpace(s)))},
+					InterpretNonJsonOutputType.CompilationError => new RunningResults(Verdict.CompilationError) { CompilationOutput = string.Join("\n", new[] { stdout, stderr }.Where(s => !string.IsNullOrWhiteSpace(s))) },
 					InterpretNonJsonOutputType.WrongAnswer => new RunningResults(Verdict.WrongAnswer) { Output = stdout, Error = stderr },
 					InterpretNonJsonOutputType.SandboxError => new RunningResults(Verdict.SandboxError) { Logs = new[] { "Не удалось распарсить результат", "Exit code: 0", $"stdout: {stdout}", $"stderr: {stderr}" } },
 					_ => new RunningResults(Verdict.SandboxError) { Logs = new[] { "Не удалось распарсить результат", "Exit code: 0", $"stdout: {stdout}", $"stderr: {stderr}" } }
