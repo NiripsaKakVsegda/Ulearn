174ac51c34ac089e7fe3da4467344e693a872464
Rozentor <rozentor2@gmail.com>
2022-05-05 23:56:18 +0500

    Fixed parentheses in DeadLinesUtilsTests, it's not supported yet.

--- src/Database.Core.Tests/Utils/DeadLinesUtilsTests.cs
+++ src/Database.Core.Tests/Utils/DeadLinesUtilsTests.cs
@@ -4,92 +4,94 @@ using Database.Models;
 using Database.Repos.DeadLines;
 using NUnit.Framework;
 
-namespace Database.Core.Tests.Utils;
-
-[TestFixture]
-public class DeadLinesUtilsTests
+namespace Database.Core.Tests.Utils
 {
-	//[0* 50*]   [100] -> 100 (always 100)
-	//[50* 0*]   [100] -> 100 (always 100)
-	[Test, Combinatorial]
-	public void HighestScoreLast_ShouldBe_AlwaysLast(
-		[Values(0, 50, 100)] int firstDeadLineScore,
-		[Values(0, 50, 100)] int secondDeadLineScore)
+
+	[TestFixture]
+	public class DeadLinesUtilsTests
 	{
-		var deadLines = new List<DeadLine>
+		//[0* 50*]   [100] -> 100 (always 100)
+		//[50* 0*]   [100] -> 100 (always 100)
+		[Test, Combinatorial]
+		public void HighestScoreLast_ShouldBe_AlwaysLast(
+			[Values(0, 50, 100)] int firstDeadLineScore,
+			[Values(0, 50, 100)] int secondDeadLineScore)
 		{
-			new()
+			var deadLines = new List<DeadLine>
 			{
-				Date = new DateTime(0),
-				ScorePercent = firstDeadLineScore
-			},
-			new()
-			{
-				Date = new DateTime(1),
-				ScorePercent = secondDeadLineScore
-			},
-			new()
-			{
-				Date = new DateTime(2),
-				ScorePercent = 100
-			},
-		};
+				new()
+				{
+					Date = new DateTime(0),
+					ScorePercent = firstDeadLineScore
+				},
+				new()
+				{
+					Date = new DateTime(1),
+					ScorePercent = secondDeadLineScore
+				},
+				new()
+				{
+					Date = new DateTime(2),
+					ScorePercent = 100
+				},
+			};
 
-		var correctDeadLine = deadLines[^1];
+			var correctDeadLine = deadLines[^1];
 
-		foreach (var deadLine in deadLines)
-			Assert.AreEqual(DeadLinesUtils.GetCurrentDeadLine(deadLines, deadLine.Date), correctDeadLine);
-	}
+			foreach (var deadLine in deadLines)
+				Assert.AreEqual(DeadLinesUtils.GetCurrentDeadLine(deadLines, deadLine.Date), correctDeadLine);
+		}
 
-	//[100 50] [0]   -> 50  (100 is overlapped by active 50, in future 0)
-	[Test]
-	public void DecreasingScore_ShouldBe_CurrentByDate()
-	{
-		var deadLines = new List<DeadLine>
+		//[100 50] [0]   -> 50  (100 is overlapped by active 50, in future 0)
+		[Test]
+		public void DecreasingScore_ShouldBe_CurrentByDate()
 		{
-			new()
+			var deadLines = new List<DeadLine>
 			{
-				Date = new DateTime(0),
-				ScorePercent = 100
-			},
-			new()
-			{
-				Date = new DateTime(1),
-				ScorePercent = 50
-			},
-			new()
-			{
-				Date = new DateTime(2),
-				ScorePercent = 0
-			},
-		};
+				new()
+				{
+					Date = new DateTime(0),
+					ScorePercent = 100
+				},
+				new()
+				{
+					Date = new DateTime(1),
+					ScorePercent = 50
+				},
+				new()
+				{
+					Date = new DateTime(2),
+					ScorePercent = 0
+				},
+			};
 
-		foreach (var deadLine in deadLines)
-			Assert.AreEqual(DeadLinesUtils.GetCurrentDeadLine(deadLines, deadLine.Date), deadLine);
-	}
+			foreach (var deadLine in deadLines)
+				Assert.AreEqual(DeadLinesUtils.GetCurrentDeadLine(deadLines, deadLine.Date), deadLine);
+		}
 
-	[Test, Combinatorial]
-	public void SameDates_ShouldBe_HighestByScore(
-		[Values(0, 50, 100)] int deadLineScore,
-		[Values(0, 50, 100)] int anotherDeadLineScore)
-	{
-		var sameDate = new DateTime(0);
-		var highestScore = Math.Max(deadLineScore, anotherDeadLineScore);
-
-		var deadLines = new List<DeadLine>
+		[Test, Combinatorial]
+		public void SameDates_ShouldBe_HighestByScore(
+			[Values(0, 50, 100)] int deadLineScore,
+			[Values(0, 50, 100)] int anotherDeadLineScore)
 		{
-			new()
-			{
-				Date = sameDate,
-				ScorePercent = deadLineScore
-			},
-			new()
-			{
-				Date = sameDate,
-				ScorePercent = anotherDeadLineScore
-			},
-		};
+			var sameDate = new DateTime(0);
+			var highestScore = Math.Max(deadLineScore, anotherDeadLineScore);
 
-		Assert.AreEqual(DeadLinesUtils.GetCurrentDeadLine(deadLines, sameDate).ScorePercent, highestScore);
+			var deadLines = new List<DeadLine>
+			{
+				new()
+				{
+					Date = sameDate,
+					ScorePercent = deadLineScore
+				},
+				new()
+				{
+					Date = sameDate,
+					ScorePercent = anotherDeadLineScore
+				},
+			};
+
+			Assert.AreEqual(DeadLinesUtils.GetCurrentDeadLine(deadLines, sameDate).ScorePercent, highestScore);
+		}
 	}
 }
