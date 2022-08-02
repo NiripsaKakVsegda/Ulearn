70a636a5f468c06ccfe28e1b5b759873ec944fc1
Rozentor <rozentor2@gmail.com>
2022-08-02 16:55:36 +0500

    Added more logging logic to GitShWorker to prevent "silent" errors

--- src/GitCourseUpdater/GitShWorker.cs
+++ src/GitCourseUpdater/GitShWorker.cs
@@ -8,7 +8,7 @@ namespace GitCourseUpdater
 	public static class GitShWorker
 	{
 		private static ILog log => LogProvider.Get().ForContext(typeof(GitRepo));
-		
+
 		public static void PerformOperation(
 			string gitOperation,
 			string gitOperationAdditionalFlags,
@@ -17,70 +17,81 @@ namespace GitCourseUpdater
 			string gitRepoUrl
 		)
 		{
-            if (!Directory.Exists(repoDir))
-            {
-                Directory.CreateDirectory(repoDir);
-            }
-            
-            var pathToProgramFiles = Environment.GetEnvironmentVariable("PROGRAMFILES");
+			if (!Directory.Exists(repoDir))
+			{
+				Directory.CreateDirectory(repoDir);
+			}
 
-            var shProcess = new Process();
-            shProcess.StartInfo = new ProcessStartInfo
-            {
-                FileName = $"{pathToProgramFiles}\\Git\\bin\\sh.exe",
-                RedirectStandardOutput = true,
-                RedirectStandardInput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                Verb = "runas",
-            };
-            
-            shProcess.OutputDataReceived += (process, output) => log.Info(output.Data);
-            
-            var error = "";
+			var pathToProgramFiles = Environment.GetEnvironmentVariable("PROGRAMFILES");
+
+			var shProcess = new Process();
+			shProcess.StartInfo = new ProcessStartInfo
+			{
+				FileName = $"{pathToProgramFiles}\\Git\\bin\\sh.exe",
+				RedirectStandardOutput = true,
+				RedirectStandardInput = true,
+				RedirectStandardError = true,
+				UseShellExecute = false,
+				CreateNoWindow = true,
+				Verb = "runas",
+			};
+			shProcess.OutputDataReceived += (process, output) =>
+			{
+				if (output.Data != null)
+					log.Info(output.Data);
+			};
+
+			var error = "";
 			var isSSHagentStopped = false;
-            shProcess.ErrorDataReceived += (process, output) =>
-            {
+			var errorRegex = new System.Text.RegularExpressions.Regex("fatal|denied|error");
+			shProcess.ErrorDataReceived += (process, output) =>
+			{
 				if (!isSSHagentStopped)
 				{
 					shProcess.StandardInput.WriteLine("eval $(ssh-agent -k)");
 					isSSHagentStopped = true;
 				}
-                if(shProcess.HasExited) return;
-                error += output.Data;
-                //exitting process
-                shProcess.StandardInput.WriteLine("exit");
-            };
-            
-            shProcess.Start();
-            shProcess.BeginOutputReadLine();
-            shProcess.BeginErrorReadLine();
-			
-            var bashCommands =
-                // get into repo directory
-                $"cd \'{repoDir}\'" +
-                //start ssh-agent for ssh keys holding, also prints process PID and keeps it
-                $" && eval $(ssh-agent -s)" +
-                //add identity to ssh-agent
-                $" && ssh-add \'{pathToPEMKey}\' " +
-                //clone repo without checkinh known_hosts with GIT_SSH_COMMAND hack
-                $" && GIT_SSH_COMMAND=\"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\" git {gitOperation} {gitRepoUrl} \'{gitOperationAdditionalFlags}\'" +
-                // removing GIT_SSH_COMMAND hack just in case
-                $" && GIT_SSH_COMMAND=\"\"" +
-                //stop ssh-agent using PID generated before
-                $" && eval $(ssh-agent -k)" +
-                //exiting sh.exe
-                $" && exit";
 
-            shProcess.StandardInput.WriteLine($"{bashCommands}");
-            
-            shProcess.WaitForExit();
-            shProcess.CancelOutputRead();
-            shProcess.CancelErrorRead();
+				if (shProcess.HasExited) return;
+				if(output.Data != null)
+				{
+					if (errorRegex.IsMatch(output.Data)) //sometimes warning get into here, we're redirecting them to log instead
+						error += output.Data + "\n";
+					else
+						log.Info(output.Data);
+				}
+				//exitting process
+				shProcess.StandardInput.WriteLine("exit");
+			};
 
-            if (shProcess.ExitCode != 0)
-                throw new Exception(error);
+			shProcess.Start();
+			shProcess.BeginOutputReadLine();
+			shProcess.BeginErrorReadLine();
+
+			var bashCommands =
+				// get into repo directory
+				$"cd \'{repoDir}\'" +
+				//start ssh-agent for ssh keys holding, also prints process PID and keeps it
+				$" && eval $(ssh-agent -s)" +
+				//add identity to ssh-agent
+				$" && ssh-add \'{pathToPEMKey}\' " +
+				//clone repo without checkinh known_hosts with GIT_SSH_COMMAND hack
+				$" && GIT_SSH_COMMAND=\"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no\" git {gitOperation} {gitRepoUrl} \'{gitOperationAdditionalFlags}\'" +
+				// removing GIT_SSH_COMMAND hack just in case
+				$" && GIT_SSH_COMMAND=\"\"" +
+				//stop ssh-agent using PID generated before
+				$" && eval $(ssh-agent -k)" +
+				//exiting sh.exe
+				$" && exit";
+
+			shProcess.StandardInput.WriteLine($"{bashCommands}");
+
+			shProcess.WaitForExit();
+			shProcess.CancelOutputRead();
+			shProcess.CancelErrorRead();
+
+			if (shProcess.ExitCode != 0 || error.Length > 0)
+				throw new Exception(error);
 		}
 
 		public static void Fetch(
