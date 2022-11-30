57cc8ca8fc4008d3924e1856fc529f91cd40960b
Rozentor <rozentor2@gmail.com>
2022-11-30 10:43:25 +0500

    Revert "Using db istead of user manager in attempt to fix operation canceled exception"
    This reverts commit c5d4907366faf8330a55a1c56d85f88588c85dc5.

--- src/uLearn.Web.Core/Extensions/LTI/LtiUtils.cs
+++ src/uLearn.Web.Core/Extensions/LTI/LtiUtils.cs
@@ -156,11 +156,7 @@ public class LtiAuthentication
 	private async Task<ApplicationUser> GetIdentityForLtiLogin(HttpContext context, LtiRequest ltiRequest, UserLoginInfo ltiLogin)
 	{
 		log.Info($"Ищу пользователя по LTI-логину {ltiLogin.LoginProvider}-{ltiLogin.ProviderKey}");
-		var userLogin = await db.UserLogins
-			.FirstOrDefaultAsync(l => l.LoginProvider == ltiLogin.LoginProvider && l.ProviderKey == ltiLogin.ProviderKey);
-		var ltiLoginUser = userLogin != null
-			? await userManager.FindByIdAsync(userLogin.UserId)
-			: null;
+		var ltiLoginUser = await userManager.FindByLoginAsync(ltiLogin.LoginProvider, ltiLogin.ProviderKey);
 		if (ltiLoginUser != null)
 		{
 			log.Info($"Нашёл LTI-логин: провайдер {ltiLogin.LoginProvider}, идентификатор {ltiLogin.ProviderKey}, он принадлежит пользователю {ltiLoginUser.UserName} (Id = {ltiLoginUser.Id})");
