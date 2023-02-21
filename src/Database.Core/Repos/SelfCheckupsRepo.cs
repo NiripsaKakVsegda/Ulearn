eb6cd2f04d47e9fecabd6d78b1be7cd738042113
Rozentor <rozentor2@gmail.com>
2023-02-21 11:26:05 +0500

    Fixed bug causing 500 on add or update in self checkups

--- src/Database.Core/Repos/SelfCheckupsRepo.cs
+++ src/Database.Core/Repos/SelfCheckupsRepo.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
-using System.Transactions;
-using Database.Extensions;
 using Database.Models;
 using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace Database.Repos;
 
@@ -29,22 +26,29 @@ public class SelfCheckupsRepo : ISelfCheckupsRepo
 
 	public async Task AddOrUpdateSelfCheckup(string userId, string courseId, Guid slideId, string checkupId, bool isChecked)
 	{
-		var checkup = new SelfCheckup
-		{
-			UserId = userId,
-			CourseId = courseId,
-			SlideId = slideId,
-			CheckupId = checkupId,
-			IsChecked = isChecked
-		};
+		await using var transaction = await db.Database.BeginTransactionAsync(IsolationLevel.Serializable);
 		
-		var executionStrategy = new NpgsqlRetryingExecutionStrategy(db, 3);
-		await executionStrategy.ExecuteAsync(async () =>
+		var checkup = await db.SelfCheckups
+			.FirstOrDefaultAsync(s => s.UserId == userId && s.CourseId == courseId && s.SlideId == slideId && s.CheckupId == checkupId);
+
+		if (checkup == null)
 		{
-			using var ts = new TransactionScope(TransactionScopeOption.Required, TimeSpan.FromSeconds(30), TransactionScopeAsyncFlowOption.Enabled);
-			db.AddOrUpdate(checkup, s => s.UserId == userId && s.CourseId == courseId && s.SlideId == slideId && s.CheckupId == checkupId);
-			await db.SaveChangesAsync();
-			ts.Complete();
-		});
+			var newCheckup = new SelfCheckup
+			{
+				UserId = userId,
+				CourseId = courseId,
+				SlideId = slideId,
+				CheckupId = checkupId,
+				IsChecked = isChecked,
+			};
+			db.SelfCheckups.Add(newCheckup);
+		}
+		else
+		{
+			checkup.IsChecked = isChecked;
+		}
+
+		await db.SaveChangesAsync();
+		await transaction.CommitAsync();
 	}
 }
