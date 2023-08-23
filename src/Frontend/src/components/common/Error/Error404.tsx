ef0c1ae702b04499e17d26bd376d2d7f690783ac
Defalt <zadnishevskii@skbkontur.ru>
2023-08-23 05:11:25 +0500

    Page404 update (#326)
    * Frontend: Error404 not found title added
    
    * Frontend: Error404 styles updated

--- src/Frontend/src/components/common/Error/Error404.tsx
+++ src/Frontend/src/components/common/Error/Error404.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Helmet } from "react-helmet";
 import notFoundImage from 'src/legacy/images/404.jpg';
 
 import styles from "./style.less";
 
-const Error404 = (): React.ReactElement => (
-	<div className={ styles.wrapper }>
-		<img src={ notFoundImage } alt="в поисках адреса страницы"
-			 className={ styles.image }/>
+
+const Error404: FC = () => {
+	return <div className={ styles.wrapper }>
+		<Helmet>
+			<title>404 Страница не найдена</title>
+		</Helmet>
+		<img
+			src={ notFoundImage } alt="в поисках адреса страницы"
+			className={ styles.image }
+		/>
 		<div className={ styles.text }>
-			<h2>СТРАНИЦА НЕ НАЙДЕНА</h2>
+			<h2 className={ styles.notFountTitle }>СТРАНИЦА НЕ НАЙДЕНА</h2>
 			<p>
 				<a href="/">Вернуться на главную</a>
 			</p>
@@ -17,7 +24,7 @@ const Error404 = (): React.ReactElement => (
 				на&nbsp;<a href="mailto:support@ulearn.me">support@ulearn.me</a>.
 			</p>
 		</div>
-	</div>
-);
+	</div>;
+};
 
 export default Error404;
