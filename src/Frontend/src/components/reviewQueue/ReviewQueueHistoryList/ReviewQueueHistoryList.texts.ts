94c2ff09db39f41f1d659bcf3731da3e0344fdd4
Ilya.Z <zadnishevskii@skbkontur.ru>
2023-08-30 01:39:48 +0500

    refactor(ReviewQueueHistoryList): mobile layout updated

--- src/Frontend/src/components/reviewQueue/ReviewQueueHistoryList/ReviewQueueHistoryList.texts.ts
+++ src/Frontend/src/components/reviewQueue/ReviewQueueHistoryList/ReviewQueueHistoryList.texts.ts
@@ -1,5 +1,11 @@
+import { ShortUserInfo } from '../../../models/users';
+import { getNameWithLastNameFirst } from '../../common/Profile/Profile';
+
 export default {
 	getScoringInfo: (score: number, maxScore: number) => `${ score } из ${ maxScore }`,
+	getReviewerInfo: (user: ShortUserInfo, withPrefix?: boolean) => withPrefix
+		? `Проверяющий: ${ getNameWithLastNameFirst(user) }`
+		: getNameWithLastNameFirst(user),
 	noSubmissionsFound: 'Не найдено проверенных работ. Попробуйте указать другую дату или поменять фильтры.',
 	noSubmissionsFoundHint: 'Здесь будут проверенные преподавателями тесты и программы студентов.'
 };
--- src/Frontend/src/components/reviewQueue/ReviewQueueHistoryList/ReviewQueueHistoryList.tsx
+++ src/Frontend/src/components/reviewQueue/ReviewQueueHistoryList/ReviewQueueHistoryList.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react';
 import { Link } from "react-router-dom";
+import { useMaxWidth } from '../../../hooks/useMaxWidth';
 import { ReviewQueueItem, ShortReviewInfo } from "../../../models/instructor";
+import MockString from "../../common/MockString/MockString";
 import { getNameWithLastNameFirst } from "../../common/Profile/Profile";
 import { CourseSlidesInfo } from "../RevoewQueue.types";
 import { getReviewQueueTimestamp } from "../utils/getReviewQueueTimestamp";
 import { getSlideTitlesByIds } from "../utils/getSlideTitlesByIds";
 import styles from './reviewQueueHistoryList.less';
 import texts from './ReviewQueueHistoryList.texts';
-import MockString from "../../common/MockString/MockString";
 
 interface Props {
 	reviewQueueItems: ReviewQueueItem[];
@@ -22,6 +23,8 @@ interface Props {
 const mockedItemsCount = 3;
 
 const ReviewQueueHistoryList: FC<Props> = (props) => {
+	const isRowLayout = useMaxWidth(450);
+
 	const renderPlaceholderItem = (key: React.Key) => {
 		const mockUserName = <>
 			<MockString length={ 10 }/> <MockString length={ 5 }/>
@@ -50,7 +53,7 @@ const ReviewQueueHistoryList: FC<Props> = (props) => {
 		</li>;
 	};
 
-	if(!props.reviewQueueItems.length) {
+	if (!props.reviewQueueItems.length) {
 		return props.loading
 			? <ul className={ styles.reviewQueueHistoryList }>
 				{ [...Array(mockedItemsCount).keys()].map(renderPlaceholderItem) }
@@ -92,15 +95,15 @@ const ReviewQueueHistoryList: FC<Props> = (props) => {
 					</span>
 				<div className={ styles.reviewerTimestampWrapper }>
 					{ checkedByUser &&
-						<span>{ getNameWithLastNameFirst(checkedByUser) }</span>
+					  <span>{ texts.getReviewerInfo(checkedByUser, isRowLayout) }</span>
 					}
 					<span className={ styles.timestamp }>{ getReviewQueueTimestamp(checkedTimestamp) }</span>
 				</div>
 			</Link>
 			{ props.showComments && !!item.reviews?.length &&
-				<ul className={ styles.reviewCommentsList }>
-					{ item.reviews.map(renderReviewComment) }
-				</ul>
+			  <ul className={ styles.reviewCommentsList }>
+				  { item.reviews.map(renderReviewComment) }
+			  </ul>
 			}
 			<div className={ styles.splitter }/>
 		</li>;
